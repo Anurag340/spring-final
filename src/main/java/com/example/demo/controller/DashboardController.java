78f661aa69d4ecaa2bd53d9d092f96a90da72622
Azura <anuragsamal123@gmail.com>
2025-03-04 01:02:34 +0530

    Update DashboardController.java

--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/dashboard")
 @CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173")
 public class DashboardController {
 
     @Autowired
