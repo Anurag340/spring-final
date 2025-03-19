78b9b4c5d7e1c2278775f2b22ca49ffa23686ad6
Anurag <anuragsamal123@gmail.com>
2025-03-19 18:19:50 +0530

    Done

--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -12,11 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/dashboard")
-@CrossOrigin(origins = { "*", "http://localhost:5173" })
+@CrossOrigin(origins = { "*", "http://localhost:5173","https://luxysmile-dashboard.vercel.app"})
 public class DashboardController {
 
     @Autowired
